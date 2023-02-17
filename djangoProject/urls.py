d3bab192a9cea9efacbbbcad517f5d1a3269e1e8
allvolload <juniormiddlehead@yandex.ru>
2023-02-17 22:25:09 +0300

    edit urls like task

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cancel/', CancelView.as_view(), name='cancel'),
     path('success/', SuccessView.as_view(), name='success'),
-    path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session')
+    path('buy/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session')
 ]
