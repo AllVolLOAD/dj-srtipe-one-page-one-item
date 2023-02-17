e0bd2043bbfe440ce7502382c32bc3f7cd26e1a3
allvolload <juniormiddlehead@yandex.ru>
2023-02-17 15:48:54 +0300

    safezone

---
+++ djangoProject/urls.py
@@ -0,0 +1,14 @@
+from django import views
+from django.contrib import admin
+from django.urls import path
+from django.contrib import admin
+from django.urls import path
+from paysystem.views import CreateCheckoutSessionView, SuccessView, CancelView, BuyView
+
+urlpatterns = [
+    path('item/1/', BuyView.as_view(), name='buy'),
+    path('admin/', admin.site.urls),
+    path('cancel/', CancelView.as_view(), name='cancel'),
+    path('success/', SuccessView.as_view(), name='success'),
+    path('create-checkout-session/<pk>/', CreateCheckoutSessionView.as_view(), name='create-checkout-session')
+]
